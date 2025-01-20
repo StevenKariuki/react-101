99d037fe0ad828cfbd7badc29fcc82130aea8403
Steven Kariuki <steven.kariuki9@outlook.com>
2025-01-20 11:32:54 +0300

    first commit

--- 1-first-react-app/04-building-blocks/Post.js
+++ 1-first-react-app/04-building-blocks/Post.js
@@ -4,14 +4,14 @@
 import FollowButton from "./FollowButton.js";
 import LikeButton from "./LikeButton.js";
 
-const postImage = "https://i.imgur.com/ZUZiJ4y.png";
-const userImage = "https://i.imgur.com/lfoiQZs.png";
+const postImage = "https://avatars.githubusercontent.com/u/129302633?s=400&u=417d8bd0442798fe01e28dd91ad108bb82cd3af2&v=4";
+const userImage = "https://avatars.githubusercontent.com/u/129302633?s=400&u=417d8bd0442798fe01e28dd91ad108bb82cd3af2&v=4";
 
 export default function Post() {
   return (
     <div className="post">
       <div className="user-info">
-        <img id="profile-img" src={userImage} alt="Profile Image" /> <p>Hipthehippocorn</p>
+        <img id="profile-img" src={userImage} alt="Profile Image" /> <p>Vi_brant</p>
         <FollowButton />
       </div>
       <img id="post-img" src={postImage} alt="Post Image" />
